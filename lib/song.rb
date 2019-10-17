22d4156e90d61894855afca35d1ca2361ac576c8
Pancho Labrador <plabradorjr@gmail.com>
2019-10-17 02:55:02 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,32 @@
+class Song
+
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@genres = []
+  @@artists = []
+
+  def initialize (name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << genre unless @@genres.include?(genre)
+    @@artists << artist unless @@artists.include?(artist)
+        
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres 
+    @@genres
+  end
+
+  def self.artists
+    @@artists 
+  end
+
+
+end
